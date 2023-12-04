88ad895890219f09c9d5ea0363bc664bb9149ba9
kingkarzy <kamachukwuma36@gmail.com>
2023-12-04 15:24:18 +0100

    edit tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,13 +18,7 @@ module.exports = {
       },
       backgroundImage: {
         'hero-pattern': "url('/src/assets/herobg.png')",
-        'hero-bg1': "url('/src/assets/bg1.gif')",
-        'hero-bg2': "url('/src/assets/bg2.gif')",
         'hero-bg3': "url('/src/assets/bg3.gif')",
-        'hero-bg4': "url('/src/assets/bg4.webp')",
-        'hero-bg5': "url('/src/assets/bg5.gif')",
-        'hero-bg6': "url('/src/assets/bg6.gif')",
-        'hero-bg7': "url('/src/assets/bg7.gif')",
         'hero-bg8': "url('/src/assets/bg8.gif')",
       },
       keyframes: {
